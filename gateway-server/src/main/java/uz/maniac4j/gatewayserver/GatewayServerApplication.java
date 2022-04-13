a3e5db78dd8999bb95db36bfcf5c909beac5f058
mahmudsalomov <mahmud.salomov.salomov@gmail.com>
2022-04-13 16:43:53 +0500

    add api gateway server

---
+++ gateway-server/src/main/java/uz/maniac4j/gatewayserver/GatewayServerApplication.java
@@ -0,0 +1,15 @@
+package uz.maniac4j.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
+
+}
