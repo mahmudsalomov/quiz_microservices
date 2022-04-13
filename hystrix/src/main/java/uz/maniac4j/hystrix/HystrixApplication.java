e96ac77e43bfad006c6747bf54260655199bc411
mahmudsalomov <mahmud.salomov.salomov@gmail.com>
2022-04-13 16:51:47 +0500

    some changes in hystrix

--- hystrix/src/main/java/uz/maniac4j/hystrix/HystrixApplication.java
+++ hystrix/src/main/java/uz/maniac4j/hystrix/HystrixApplication.java
@@ -2,8 +2,14 @@ package uz.maniac4j.hystrix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
 public class HystrixApplication {
 
     public static void main(String[] args) {
