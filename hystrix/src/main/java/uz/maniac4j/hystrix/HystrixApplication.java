0ecc56d52a9dab7ea9280e52619e712c413dc3c7
mahmudsalomov <mahmud.salomov.salomov@gmail.com>
2022-04-13 16:48:47 +0500

    add hystrix

---
+++ hystrix/src/main/java/uz/maniac4j/hystrix/HystrixApplication.java
@@ -0,0 +1,13 @@
+package uz.maniac4j.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixApplication.class, args);
+    }
+
+}
---
+++ hystrix/src/test/java/uz/maniac4j/hystrix/HystrixApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.maniac4j.hystrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
